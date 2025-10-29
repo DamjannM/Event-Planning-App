6f6efecbc9efb84679fcf0ebf2a3961fe46e9c4a
DamjannM <damjanmilosavljevic@gmail.com>
2025-10-29 09:06:05 +0100

    event notification toast

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,7 @@ import { Server } from "socket.io";
 import authRoutes from "./routes/authRoutes";
 import eventRoutes from "./routes/eventRoutes";
 import authMiddleware from "./middleware/authMiddleware";
+import db from './db'
 
 dotenv.config();
 
@@ -29,6 +30,17 @@ io.on("connection", (socket) => {
     console.log("Client disconnected:", socket.id);
   });
 });
+
+setInterval(() => {
+  const now = Date.now();
+  const upcoming = db.prepare(
+    "SELECT * FROM events WHERE timestamp BETWEEN ? AND ?"
+  ).all(now, now + 10 * 60 * 1000); 
+
+  upcoming.forEach(event => {
+    io.emit("eventReminder", event);
+  });
+}, 60 * 1000)
 
 export const notifyAllClients = (eventName: string, data: any) => {
   io.emit(eventName, data);
