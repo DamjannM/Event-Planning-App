bc4329e9f14278df4d40920dc80a916d0ce961e4
DamjannM <damjanmilosavljevic@gmail.com>
2025-11-10 13:37:08 +0100

    Changed background of application

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,7 +5,7 @@ type HeaderProps = {
 };
 
 export function Header({ handleLogOut }: HeaderProps){
-    return <div className="bg-white h-16 items-center text-sm sm:text-base justify-between flex !m-2 !p-2  rounded-2xl">
+    return <div className="bg-violet-200 h-16 items-center text-sm sm:text-base justify-between flex !m-2 !p-2 rounded-2xl">
         <img src={"/logo.jpg"} alt="Event logo" className="h-10 w-auto" />
         <span className="text-xl font-semibold text-blue-950">Event Planner</span>
         <Button
